28ba22e8f4e58144c3fbfbf52b130cf22387b975
Thorben Jensen <jensen.thorben@gmail.com>
2017-11-10 17:38:07 +0100

    adding time line plots

--- src/bus/03_plot_data.R
+++ src/bus/03_plot_data.R
@@ -3,5 +3,20 @@
 library(dplyr)
 library(ggplot2)
 
-# 
+# LOAD DATA
+df <- 
+  read.csv("data/processed/bus2.csv") %>%
+  mutate(timestamp = as.POSIXct(timestamp))
 
+# check for spatial outliers: scatter plot of X and Y
+ggplot(df, aes(X, Y)) +
+  geom_point(alpha = .2)
+
+# time line
+ggplot(df, aes(timestamp, Ein)) +
+  geom_line()
+
+# time line, aggregated by hour
+# TODO
+ggplot(df, aes(timestamp, Ein)) +
+  geom_line()
