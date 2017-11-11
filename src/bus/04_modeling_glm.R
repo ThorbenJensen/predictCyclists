af83269816679b40933b4bad7b8babd6c48dbb9e
Thomas Kluth <t.kluth@posteo.de>
2017-11-11 14:26:43 +0100

    added hbf ID for reading in data

--- src/bus/04_modeling_glm.R
+++ src/bus/04_modeling_glm.R
@@ -1,15 +1,23 @@
 # MODELING WITH CLASSICAL GLM
 
 library(dplyr)
-library(glm)
+library(glm2)
 library(MASS)
 library(boot)
+library(sjPlot)
 
 # LOAD DATA
+#hstID = "43901" # sophienstraße
+hstID = "41000" # hauptbahnhof
+
+# load data
 df <- 
-  read.csv("data/processed/bus2.csv") %>%
+  read.csv(paste0("data/processed/all_", hstID, "_enhanced.csv")) %>%
   mutate(month = as.factor(month)) %>%
-  mutate(hour = as.factor(hour))
+  mutate(hour = as.factor(hour)) %>%
+  mutate(bank_holiday = as.factor(bank_holiday)) %>%
+  mutate(event_send = as.factor(event_send)) %>%
+  mutate(rush_hour = as.factor(rush_hour))
 
 # train model
 model <- glm.nb(Ein 
@@ -19,6 +27,14 @@ model <- glm.nb(Ein
                 # + rush_hour
                 ,
                 data = df)
+
+# plot model
+plot(model)
+# predictions compared to data
+sjp.glm(model, type = "pred", vars = c("month"))
+sjp.glm(model, type = "pred", vars = c("weekday"))
+# marginal effects
+sjp.glm(model, type = "eff",  vars = c("month"))
 
 # measure RMSE
 mse <- cv.glm(df, model, K = 7)$delta
